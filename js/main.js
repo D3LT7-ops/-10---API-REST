fae3153023a4044af53ae07e820248ef07358d1b
D3LT7-ops <hrgs@aluno.ifnmg.edu.br>
2025-08-17 23:46:00 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
 
 const API_CONFIG = {
     KEY: '99a15dbcafb7db271f431e7751086382', 
-    BASE_URL: 'https://api.weatherstack.com/current.json',
-    FORECAST_URL: 'https://api.weatherstack.com/forecast.json',
-    HISTORY_URL: 'https://api.weatherstack.com/historical.json'
+    BASE_URL: 'https://api.weatherstack.com/current',
+    FORECAST_URL: 'https://api.weatherstack.com/forecast',
+    HISTORY_URL: 'https://api.weatherstack.com/historical'
 };
 
 // Para demonstração - simula uma API fake para favoritos
